6b5eea35e9371102f30aa61e0148d423a856668e
Caspar MacRae <caspar@matterhorninvestment.com>
2012-05-29 10:07:19 +0100

    Merged dataview plugin from 'grid' branch

--- ui/jquery.ui.dataview.js
+++ ui/jquery.ui.dataview.js
@@ -18,18 +18,14 @@ $.widget( "ui.dataview", {
 		filter: null
 	},
 
-	// TODO subwidgets override _create, should we force them to call _super("_create")?
-	// or is there a way to have a constructor along with _create?
-	// _init is probably safe here, as this shouldn't get called as a widget anyway
 	_init: function() {
+		// normalize options
+		this.option(this.options);
 		this.result = [];
 	},
 
-	// TODO this needs to be applied to init options as well, to make sort: "prop" work
 	_setOption: function( key, value ) {
 		// reset offset to 0 when changing limit
-		// TODO actually only necessary when offset > offset + new limit
-		// in other words, when on a page that won't exist anymore after the limit change
 		if ( key === "paging" && value.limit !== this.options.paging.limit ) {
 			value.offset = 0;
 		}
@@ -49,13 +45,14 @@ $.widget( "ui.dataview", {
 				value = null;
 			}
 		}
-		this._super( "_setOption", key, value );
+		this._super( key, value );
 	},
 
 	page: function( pageIndex ) {
 		var limit = this.options.paging.limit;
 		if ( pageIndex !== undefined ) {
 			this.option( "paging.offset", pageIndex * limit - limit );
+			return this;
 		}
 		return Math.ceil( this.options.paging.offset / limit + 1 );
 	},
@@ -64,20 +61,26 @@ $.widget( "ui.dataview", {
 		return Math.ceil( this.totalCount / this.options.paging.limit );
 	},
 
-	refresh: function( callback ) {
-		if ( callback ) {
-			this.element.one( "dataviewresponse", callback );
-		}
+	// TODO need reasonable signatures/names for callback and error.
+	refresh: function( callback, error ) {
 		this._trigger( "request" );
 
 		var request = $.extend( {}, this.options, {
 			page: this.page()
 		});
+
 		var that = this;
 		this.options.source( request, function( data, totalCount ) {
 			that.totalCount = parseInt(totalCount, 10);
 			$.observable( that.result ).replaceAll( data );
 			that._trigger( "response" );
+			if (callback) {
+				callback.apply(that, arguments);
+			}
+		}, function () {
+			if (error) {
+				error.apply(that, arguments);
+			}
 		});
         return this;
 	}
